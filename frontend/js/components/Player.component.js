6b7befeb3d7483800efb0b5ebe36427fa85bc2e0
Nicolas Bouliane <contact@nicolasbouliane.com>
2017-05-02 17:48:39 +0200

    Close button for player

--- frontend/js/components/Player.component.js
+++ frontend/js/components/Player.component.js
@@ -1,7 +1,12 @@
 const PlayerComponent = Vue.component('player', {
   props: ['part'],
+  methods: {
+    close: function() {
+      this.$router.push({ name: 'movies' });
+    }
+  },
   template: `
-    <div class="text-center player">
+    <div class="text-center player" v-on:click.self="close">
       <video id="video" controls autoplay>
         <source :src="part.videoUrl" type="video/mp4">
         <track label="English" kind="captions" srclang="en" :src="part.subtitlesUrl" default>
