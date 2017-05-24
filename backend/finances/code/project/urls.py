6090d65929c7d7296cced56947d2de54234903a2
Nicolas Bouliane <contact@nicolasbouliane.com>
2017-05-24 22:49:28 +0200

    Created shell for the backend

---
+++ backend/finances/code/project/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+]
+urlpatterns += staticfiles_urlpatterns()
---
+++ backend/finances/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+python manage.py migrate                  # Apply database migrations
+python manage.py collectstatic --noinput  # Collect static files
+
+# Prepare log files and start outputting logs to stdout
+touch /srv/logs/gunicorn.log
+touch /srv/logs/access.log
+tail -n 0 -f /srv/logs/*.log &
+
+# Start Gunicorn processes
+echo Starting Gunicorn.
+exec gunicorn project.wsgi:application \
+    --name hello_django \
+    --bind 0.0.0.0:80 \
+    --workers 3 \
+    --log-level=info \
+    --log-file=/srv/logs/gunicorn.log \
+    --access-logfile=/srv/logs/access.log \
+    "$@"
