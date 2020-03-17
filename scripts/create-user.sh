1769eb66fe9d831b18f5d5f55daaef56678313a0
Nicolas Bouliane <contact@nicolasbouliane.com>
2020-03-17 10:41:12 +0100

    Added pihole

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+echo "Configuring general server admin account"
 docker-compose exec backend python3 manage.py createsuperuser
+
+echo "Configuring pihole admin password"
+docker-compose exec pihole sudo pihole -a -p
