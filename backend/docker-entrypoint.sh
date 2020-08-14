5f90a2600a3f79b280dd4eb86d932bfe36f33d7b
Nicolas Bouliane <contact@nicolasbouliane.com>
2020-08-14 14:28:48 +0300

    Fixed migrations

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -3,8 +3,8 @@
 # Wait for database
 until nc -z db 5432; do echo Waiting for PostgreSQL; sleep 1; done
 
-python manage.py migrate                  # Apply database migrations
-python manage.py collectstatic --noinput  # Collect static files
+python3 manage.py migrate                  # Apply database migrations
+python3 manage.py collectstatic --noinput  # Collect static files
 
 # Prepare log files and start outputting logs to stdout
 touch /srv/logs/gunicorn.log
