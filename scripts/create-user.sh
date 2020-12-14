e96b8dc80479490502b8acb7dadbb57469f34da5
Nicolas Bouliane <contact@nicolasbouliane.com>
2020-12-14 12:17:09 +0100

    Removed pihole

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 echo "Configuring general server admin account"
 docker-compose exec backend python3 manage.py createsuperuser
-
-echo "Configuring pihole admin password"
-docker-compose exec pihole sudo pihole -a -p
