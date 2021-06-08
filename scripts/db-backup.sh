474adf51974b301c6d26a7e2b9ce91feed30a01d
Nicolas Bouliane <contact@nicolasbouliane.com>
2021-06-08 21:43:59 +0200

    Added missing scripts

---
+++ scripts/db-backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+echo -e "\n\033[1mBacking up the database to ${1}\033[0m"
+docker exec -t $(docker-compose ps -q db) pg_dumpall -c -U postgres -f /tmp/db-dump.sql
+docker cp $(docker-compose ps -q timeline-db):/tmp/db-dump.sql "$1"
+docker exec -t $(docker-compose ps -q db) rm /tmp/db-dump.sql
+echo "Done."
