3fe2f9bd312c2aaa09e95d13142d393e2ac8824a
Nicolas Bouliane <contact@nicolasbouliane.com>
2023-12-28 15:31:56 +0100

    Timeline: Exclude stuff

--- timeline/run.sh
+++ timeline/run.sh
@@ -4,6 +4,11 @@ while true; do
         --url "/timeline" \
         --maps-key "$GOOGLE_MAPS_API_KEY" \
         --output /var/timeline/output \
+        --exclude .git/* \
+        --exclude .trashed* \
+        --exclude Settings/* \
+        --exclude Documents/Manuals/* \
+        --exclude Documents/Projects/* \
 	/media/timeline
     sleep 1800
 done
