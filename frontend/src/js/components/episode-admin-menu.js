3ca2d987b21260288a6faae8e21632d7dbdcd87e
Nicolas Bouliane <contact@nicolasbouliane.com>
2024-09-22 11:09:03 +0200

    Add batch deletion of episodes and seasons

--- frontend/src/js/components/episode-admin-menu.js
+++ frontend/src/js/components/episode-admin-menu.js
@@ -9,28 +9,42 @@ export default Vue.component('admin-menu', {
   computed: {
   },
   methods: {
-    deleteEpisode: function() {
+    deleteEpisode: function(episode) {
       this.$store.dispatch('movies/deleteEpisode', {
         tmdbId: this.movie.tmdbId,
-        episodeId: this.episode.id,
+        episodeId: episode.id,
       });
       this.showDeleteEpisode = false;
     },
-    deleteOriginalFile: function() {
+    deleteOriginalFile: function(episode) {
       this.$store.dispatch('movies/deleteOriginalFile', {
         tmdbId: this.movie.tmdbId,
-        episodeId: this.episode.id,
+        episodeId: episode.id,
       });
       this.showDeleteOriginalFile = false;
+    },
+    deleteSeason: function(){
+      this.movie.episodeList
+        .filter(ep => ep.season === this.episode.season)
+        .forEach(ep => this.deleteEpisode(ep))
+    },
+    deleteAllEpisodes: function(){
+      this.movie.episodeList.forEach(ep => this.deleteEpisode(ep));
     },
   },
   template: `
     <div>
-      <a class="button" href="#" @click.prevent="deleteEpisode" v-if="showDeleteEpisode">
+      <a class="button" href="#" @click.prevent="deleteEpisode(episode)" v-if="showDeleteEpisode">
         <i class="fas fa-trash-alt"></i> Delete and remove from library
       </a>
-      <a class="button" href="#" @click.prevent="deleteOriginalFile" v-if="episode.isConverted && episode.originalVideoPreserved && showDeleteOriginalFile">
+      <a class="button" href="#" @click.prevent="deleteOriginalFile(episode)" v-if="episode.isConverted && episode.originalVideoPreserved && showDeleteOriginalFile">
         <i class="fas fa-broom"></i> Delete original, keep converted version
+      </a>
+      <a class="button" href="#" @click.prevent="deleteSeason" v-if="movie.episodeList && showDeleteEpisode">
+        <i class="fas fa-broom"></i> Delete season
+      </a>
+      <a class="button" href="#" @click.prevent="deleteAllEpisodes" v-if="movie.episodeList && showDeleteEpisode">
+        <i class="fas fa-broom"></i> Delete show
       </a>
     </div>
   `
