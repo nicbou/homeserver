b7fcba3597bdec1b4b6c7320330c68c8ed5ba1d7
Nicolas Bouliane <contact@nicolasbouliane.com>
2025-11-23 13:56:34 +0100

    Fix shellcheck errors

--- gps-logger/docker-entrypoint.sh
+++ gps-logger/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/bash
 set -e
 
 # Set permissions
--- scripts/copy-production-db.sh
+++ scripts/copy-production-db.sh
@@ -7,8 +7,8 @@ today=$(date "+%Y-%m-%d")
 
 echo -e "\033[1;32mCopying production DB\033[0m"
 
-scp -P 2200 root@home.nicolasbouliane.com:/var/homeserver/db-backups/backup-${today}.sql backup-${today}.sql
-bash ${scripts_dir}/restore-db.sh backup-${today}.sql
+scp -P 2200 "root@home.nicolasbouliane.com:/var/homeserver/db-backups/backup-${today}.sql" "backup-${today}.sql"
+bash "${scripts_dir}/restore-db.sh backup-${today}.sql"
 
 echo -e "\033[1;32mCopying production uploads\033[0m"
 
