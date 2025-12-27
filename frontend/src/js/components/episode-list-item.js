46308a111130a023a8cfec9d81ea076c2d7cc715
Nicolas Bouliane <contact@nicolasbouliane.com>
2025-12-27 22:34:15 +0100

    episode-list-item: Use buttons instead of links, remove invalid URL

--- frontend/src/js/components/episode-list-item.js
+++ frontend/src/js/components/episode-list-item.js
@@ -19,15 +19,6 @@ export default Vue.component('episode-list-item', {
     }
   },
   methods: {
-    playEpisode(){
-      this.$router.push({
-        name: 'episode',
-        params: {
-          tmdbId: this.movie.tmdbId,
-          episodeId: this.episode.id,
-        },
-      });
-    },
     deleteEpisode(){
       this.$store.dispatch('movies/deleteEpisode', {
         tmdbId: this.movie.tmdbId,
@@ -50,26 +41,26 @@ export default Vue.component('episode-list-item', {
   template: `
     <div class="episode collapsible" :class="{expanded: expanded}">
       <div class="collapsible-header">
-        <a title="Mark as seen" class="button" href="#" v-if="!episode.isWatched" @click.prevent="markEpisodeAsWatched">
+        <button title="Mark as seen" class="button" v-if="!episode.isWatched" @click.prevent="markEpisodeAsWatched">
           <i class="far fa-circle"></i>
-        </a>
-        <a title="Mark as not seen" class="button" href="#" v-if="episode.isWatched" @click.prevent="markEpisodeAsUnwatched">
+        </button>
+        <button title="Mark as not seen" class="button" v-if="episode.isWatched" @click.prevent="markEpisodeAsUnwatched">
           <i class="fas fa-check-circle"></i>
-        </a>
+        </button>
         <span class="title">Episode {{ episode.episode }}</span>
         <div class="button-group horizontal">
-          <a class="button icon-only" v-if="episode.isConverting" title="Video is converting for web playback">
+          <button class="button icon-only" v-if="episode.isConverting" title="Video is converting for web playback">
             <i class="fa fa-spinner fa-spin"></i>
-          </a>
-          <a title="Play in browser" class="button icon-only" href="#" v-if="episode.isConverted" :href="episode.playbackUrl" @click.prevent="playEpisode">
+          </button>
+          <router-link title="Play in browser" class="button icon-only" :to="{ name: 'episode', params: { tmdbId: movie.tmdbId, episodeId: episode.id }}">
             <i class="fas fa-play"></i>
-          </a>
-          <a title="Download movie and subtitles" class="button icon-only" :class="{selected: downloadMenuVisible}" href="#" @click.prevent="downloadMenuVisible = !downloadMenuVisible;adminMenuVisible = false">
+          </router-link>
+          <button title="Download movie and subtitles" class="button icon-only" :class="{selected: downloadMenuVisible}" @click="downloadMenuVisible = !downloadMenuVisible;adminMenuVisible = false">
             <i class="fas fa-download"></i>
-          </a>
-          <a title="Administrator options" class="button icon-only" :class="{selected: adminMenuVisible}" href="#" v-if="isAdmin" @click.prevent="adminMenuVisible = !adminMenuVisible;downloadMenuVisible = false">
+          </button>
+          <button title="Administrator options" class="button icon-only" :class="{selected: adminMenuVisible}" v-if="isAdmin" @click="adminMenuVisible = !adminMenuVisible;downloadMenuVisible = false">
             <i class="fas fa-ellipsis-h"></i>
-          </a>
+          </button>
         </div>
       </div>
       <download-menu
