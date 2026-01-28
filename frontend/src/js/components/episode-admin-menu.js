b25b70ca15c3c44048045331c66bd5d201dabd77
Nicolas Bouliane <contact@nicolasbouliane.com>
2026-01-28 23:33:08 +0100

    episode-admin-menu: Add button to delete original

--- frontend/src/js/components/episode-admin-menu.js
+++ frontend/src/js/components/episode-admin-menu.js
@@ -27,6 +27,14 @@ export default Vue.component('admin-menu', {
     deleteAllEpisodes(){
       this.movie.episodeList.forEach(this.deleteEpisode);
     },
+    deleteOriginalVideo(episode) {
+      this.movie.episodeList.filter(e => e.hasOriginalVersion && (!episode || e.id === episode.id)).forEach(episode => {
+        this.$store.dispatch('movies/deleteOriginalVideo', {
+          tmdbId: movie.tmdbId,
+          episodeId: episode.id,
+        });
+      });
+    },
     postDelete(){
       if(this.movie.episodeList.length === 0){
         this.$router.push({ name: 'movies' });
@@ -37,6 +45,7 @@ export default Vue.component('admin-menu', {
     <div>
       <button class="button" v-if="canDelete && episode" @click="deleteEpisode(episode)"><i class="fas fa-trash-alt"></i> Delete this episode</button>
       <button class="button" v-if="canDelete && episode" @click="deleteSeason"><i class="fas fa-trash-alt"></i> Delete this season</button>
+      <button class="button" v-if="canDelete && (!episode || episode.hasOriginalVersion)" @click="deleteOriginalVideo(episode)"><i class="fas fa-trash-alt"></i> Delete original, keep converted version</button>
       <button class="button" v-if="canDelete && isTvShow" @click="deleteAllEpisodes"><i class="fas fa-trash-alt"></i> Delete all episodes</button>
       <button class="button" v-if="canDelete && !isTvShow" @click="deleteAllEpisodes"><i class="fas fa-trash-alt"></i> Delete this movie</button>
     </div>
